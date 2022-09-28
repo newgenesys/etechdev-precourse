a2eecbdc9bb88a941ca3579d3651cc3911d466b6
olouge <olouge@ngtltd.com>
2022-09-28 03:23:37 +0100

    frontend code updates for examples

---
+++ frontend/examples/myjavascript.js
@@ -0,0 +1,93 @@
+// alert("I am alerting you from myjavascript.js file");
+
+document.getElementById("demo").innerHTML = "This is a new content for this paragraph loaded with JavaScript";
+// 50;
+// 100;
+// 30;
+
+// 30+20;
+
+//Arithmetic operators: +, -, /, *, %
+
+//Using it directly
+
+// apple = 20;
+// b = 30;
+
+// c = apple + b;
+
+// alert(c);
+
+
+// Using declarators (var, let and const)
+
+var myName = "Olouge"; //local scope
+
+let myAge = 30;
+
+const PI = 3.14;
+
+// alert("Name: "+myName +", Age:" + myAge);
+
+var x = 20;
+
+var y = "20.0";
+
+//OR is || two-pipes
+//AND is && ampersand
+
+//NOT is ! exclamation
+
+
+if(x==y && x===y){
+    alert("equal by value and type");
+}
+
+// if(x === y){
+//     alert("equal by value and type");
+// }
+
+// Scope of a variable
+
+
+
+
+console.log("Hello this is console message");
+
+
+//Data Types
+
+//Number, Strings, Booleans, Arrays, Objects
+
+//Numbers: Integers, decimal numbers, etc
+20.0
+20
+
+//String: Collection of letters and symmbols written in double or single quotes
+"Olouge 2022"
+
+
+//Booleans: 0 and 1
+true
+false
+
+
+//Arrays: Composite variables that hold multiple types
+
+items = ["item1", 20, true, 20.1];
+
+//Objects: representation of a real world entity
+
+car = {name: "Volvo", model: "V800", color: "white" };
+
+//Function is a block of code to be executed as one and may return a value
+
+function myFunction(param1, param2, param3, /*.....*/){
+    //Code to be executed
+}
+
+/*
+kfdfdgf
+fdfdkfk
+dskjdkfdf
+*/
